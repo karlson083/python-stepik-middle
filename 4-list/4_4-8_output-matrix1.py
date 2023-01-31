a418221b85cb1261938738b810b8bbe363814b52
karlson083 <karlson083@gmail.com>
2023-01-31 18:05:43 +0300

    20230131 1805output matrix OK

--- 4-list/4_4-8_output-matrix1.py
+++ 4-list/4_4-8_output-matrix1.py
@@ -2,20 +2,40 @@ import os
 clear = lambda: os.system('cls')
 clear()
 
-list_in = 'a b v'.split()
+n, m = int(input()), int(input())
+start_list =[]
 
-#list_in = input().split()
+def create_matrix_from_list(input_list, n1, m1):
+    output_list = []
+    cnt = 0
+    for _ in range(n1):
+        output_list.append([input_list[cnt]])
+        cnt += 1
+        for _ in range(m1-1):
+            output_list[-1].append(input_list[cnt])
+            cnt += 1
+    return output_list
+
+def input_list_with_enter(count):
+    output_list = []
+    for _ in range(count):
+        output_list.append(input())
+    return output_list
+
+def print_matrix_rows(matrix, rows, cols):
+    for r in range(rows):
+        for c in range(cols):
+            print(matrix[r][c], end=' ')
+        print()
+
+def print_matrix_cols(matrix, rows, cols):
+    for c in range(cols):
+        for r in range(rows):
+            print(matrix[r][c], end=' ')
+        print()
 
 
-def sub_lists(list1):
-    result = [[]]
-    for i in range(0, len(list1)):
-        for x in range(0, len(list1) - i ):
-            result.append(x : i + x + 1])
-    return result
-
-print(sub_lists(list_in))
-
+print_matrix_rows(create_matrix_from_list(input_list_with_enter(n*m) ,n ,m), n, m)
 
 ###### v2
 
