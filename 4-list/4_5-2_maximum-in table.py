b9183aa443e8ba4847ade317249da85cb76e6863
karlson083 <karlson083@gmail.com>
2023-02-06 23:29:09 +0300

    20230206 2327 4-5-2-maximum-in-table OK

--- 4-list/4_5-2_maximum-in table.py
+++ 4-list/4_5-2_maximum-in table.py
@@ -29,3 +29,14 @@ print(*(maximum_in_matrix_index(create_matrix_classic_n(n))))
 
 ###### v2
 
+
+n, m = int(input()), int(input())
+matrix = [[int(i) for i in input().split()] for _ in range(n)]
+row, col = 0, 0
+    
+for i in range(n):
+    for j in range(m):
+        if matrix[i][j] > matrix[row][col]:
+            row,col = i, j
+            
+print(row, col)
