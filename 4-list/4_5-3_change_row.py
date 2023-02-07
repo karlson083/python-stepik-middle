4d0f9649b602c21488a82423f7a8d9dd00e4cbef
karlson083 <karlson083@gmail.com>
2023-02-07 22:45:03 +0300

    20230207 2244 4-5-3-change-rom

--- 4-list/4_5-3_change_row.py
+++ 4-list/4_5-3_change_row.py
@@ -17,15 +17,14 @@ def create_matrix_classic_n(n):
 matrix_1 = create_matrix_classic_n(n)
 matrix_change = create_matrix_classic_n(1)
 
-print(matrix_change)
 matrix_out = []
 for k in range(n):
     matrix_out.append([])
     for l in range(len(matrix_1[k])):
-        if l == matrix_change[0]:
-            matrix_out[-1].append(matrix_1[k][matrix_change[1]])
-        elif l == matrix_change[1]:
-            matrix_out[-1].append(matrix_1[k][matrix_change[0]])
+        if l == matrix_change[0][0]:
+            matrix_out[-1].append(matrix_1[k][matrix_change[0][1]])
+        elif l == matrix_change[0][1]:
+            matrix_out[-1].append(matrix_1[k][matrix_change[0][0]])
         else:
             matrix_out[-1].append(matrix_1[k][l])
 for m in matrix_out:
@@ -33,3 +32,12 @@ for m in matrix_out:
 
 ###### v2
 
+n, m = int(input()), int(input())
+matrix = [input().split() for _ in range(n)]
+col1, col2 = [int(i) for i in input().split()]
+
+for i in range(n):
+    matrix[i][col1], matrix[i][col2] = matrix[i][col2], matrix[i][col1]
+
+for row in matrix:
+    print(*row)
