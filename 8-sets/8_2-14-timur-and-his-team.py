112697f3c194cc0b921494f40c1edd4d418e5b02
karlson083 <karlson083@gmail.com>
2023-02-24 21:30:30 +0300

    20230224 2128 8-2-14-timur-and-his-team OK

--- 8-sets/8_2-14-timur-and-his-team.py
+++ 8-sets/8_2-14-timur-and-his-team.py
@@ -2,7 +2,8 @@ import os, math
 clear = lambda: os.system('cls')
 clear()
 
-
+n, m, k, x, y ,z = [int(input()) for _ in range(6)]
+print(n - x + m + k - y + z)
 
 
 ###### v2
---
+++ 8-sets/8_2-15-books-for-read.py
@@ -0,0 +1,8 @@
+import os, math
+clear = lambda: os.system('cls')
+clear()
+
+
+
+
+###### v2
