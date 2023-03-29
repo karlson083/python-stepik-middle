837c849b7b6c2599854a3a5ee4a172abee4288c6
karlson083 <karlson083@gmail.com>
2023-03-29 15:22:05 +0300

    8-3 tmp-commit

---
+++ 8-sets/8_3-test.py
@@ -0,0 +1,36 @@
+import os, math
+clear = lambda: os.system('cls')
+clear()
+
+
+numbers = {2, 4, 6, 8, 10}
+languages = {'Python', 'C#', 'C++', 'Java'}
+
+info = {'Timur', 1992, 61.5}
+myset = set() 
+
+numbers = {2, 4, 6, 8, 10}
+languages = {'Python', 'C#', 'C++', 'Java'}
+mammals = {'cat', 'dog', 'fox', 'elephant'}
+
+print(numbers)
+print(languages)
+print(mammals)
+
+
+myset1 = {2, 2, 4, 6, 6}
+myset2 = set([1, 2, 2, 3, 3, 4, 4, 5, 5])
+myset3 = set('aaaaabbbbccccddd')
+
+print(len(myset1))
+print(len(myset2))
+print(len(myset3))
+
+
+
+
+
+
+###### v2
+
+
