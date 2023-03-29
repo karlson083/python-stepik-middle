3de4b3d178c7544c424c94ed26a0972924138333
karlson083 <karlson083@gmail.com>
2023-03-29 15:28:52 +0300

    8-4-5 and 8-4-6 OK

---
+++ 8-sets/8_4-5.py
@@ -0,0 +1,17 @@
+import os, math
+clear = lambda: os.system('cls')
+clear()
+
+
+
+numbers = {1.414, 12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324, 2.718, 1.324}
+
+print(min(numbers) + max(numbers))
+
+
+
+
+
+###### v2
+
+
