392e42402358de9a645797bf75d1b1d97673bf74
karlson083 <karlson083@gmail.com>
2023-04-05 16:41:13 +0300

    8-3-test change commit

--- 8-sets/8_3-test.py
+++ 8-sets/8_3-test.py
@@ -3,28 +3,13 @@ clear = lambda: os.system('cls')
 clear()
 
 
-numbers = {2, 4, 6, 8, 10}
-languages = {'Python', 'C#', 'C++', 'Java'}
+word = 'beegeek'
+set1 = set(word*3)
+print(set1)
+set2 = set(word[::-1]*2 + 'stepik')
+print(set2)
 
-info = {'Timur', 1992, 61.5}
-myset = set() 
-
-numbers = {2, 4, 6, 8, 10}
-languages = {'Python', 'C#', 'C++', 'Java'}
-mammals = {'cat', 'dog', 'fox', 'elephant'}
-
-print(numbers)
-print(languages)
-print(mammals)
-
-
-myset1 = {2, 2, 4, 6, 6}
-myset2 = set([1, 2, 2, 3, 3, 4, 4, 5, 5])
-myset3 = set('aaaaabbbbccccddd')
-
-print(len(myset1))
-print(len(myset2))
-print(len(myset3))
+print(set1 < set2)
 
 
 
